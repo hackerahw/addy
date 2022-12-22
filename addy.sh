ae7b0fef5da50a3c3dc5ac49cdd3a52e9edd6832
addy stark <121247744+hackerahw@users.noreply.github.com>
2022-12-22 17:05:38 +0000

    Add files via upload

---
+++ addy.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+echo "any-hacker-world"
+
+echo "before using this tool please make
+sure you are in @ super user mood on your terminal @"
+
+sudo apt-get update  -y
+sudo apt-get upgrade  -y
+sudo apt-get install python -y 
+sudo apt-get install python2 -y
+sudo apt-get install python3 -y
+sudo apt-get install php -y
+sudo apt-get install ruby -y
+sudo apt-get install curl -y
+sudo apt-get install wget -y
+sudo apt-get install git -y 
+sudo apt-get install perl -y
